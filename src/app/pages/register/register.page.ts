633ef650e3273993332fc43db038e6a2042851e4
lukaspgtu <lukaspgtu14@gmail.com>
2021-11-19 14:20:36 -0300

    up

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+  @ViewChild(IonContent) content: IonContent;
+
+  public messages: any[] = [];
+
+  public typing: boolean;
+
+  public typingClass: string;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.step1();
+
   }
+
+  public step1() {
+
+    this.typing = true;
+
+    this.typingClass = 'typing top';
+
+    setTimeout(() => {
+      
+      this.messages.push({
+        class: 'chat-left top',
+        text: 'Olá! 😊'
+      });
+
+      this.typingClass = 'typing';
+
+    }, 1000);
+
+    setTimeout(() => {
+      
+      this.messages.push({
+        class: 'chat-left',
+        text: 'Que bom ver você aqui!'
+      });
+
+    }, 3500);
+
+    setTimeout(() => {
+      
+      this.messages.push({
+        class: 'chat-left',
+        text: 'Sua conta será atrelada ao seu CPF.'
+      });
+
+    }, 6000);
+
+    setTimeout(() => {
+      
+      this.messages.push({
+        class: 'chat-left',
+        text: 'Por isso, digite o número do seu CPF.'
+      });
+
+      this.typing = false;
+
+    }, 9000);
+
+  }
+
 
 }
